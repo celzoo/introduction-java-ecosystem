f702854c1bffc7d0ea69967803004f3c85cff7f9
celzoo <celzoaveirofilho@gmail.com>
2021-10-29 17:42:13 -0300

    Introduction to java ecosystem

---
+++ src/com/dio/Myfirstprogram.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import com.dio.base.Order;
+
+public class Myfirstprogram {
+
+	public static void main(String[] args) {
+		Order order = new Order ("1234");
+		System.out.println(order);
+
+	}
+
+}
---
+++ src/com/dio/base/Order.java
@@ -0,0 +1,18 @@
+package com.dio.base;
+
+public class Order {
+	private String code;
+
+	public Order(String code) {
+		super();
+		this.code = code;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [code=" + code + "]";
+	}
+	
+	
+
+}
